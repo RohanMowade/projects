0769ea36d12a79ef16a7c7595b573d4d933bdb0e
RohanMowade <146569356+RohanMowade@users.noreply.github.com>
2025-10-30 09:03:56 +0530

    Add files via upload

---
+++ Day02/ConditionalRendering/App.jsx
@@ -0,0 +1,11 @@
+import UserGreeting from "./UserGreeting";
+
+function App() {
+  return(
+    <>
+     <UserGreeting isLoggedIn = {true} username = "rohan mowade"></UserGreeting>
+    </>
+  );
+}
+
+export default App;
---
+++ Day02/ConditionalRendering/UserGreeting.jsx
@@ -0,0 +1,9 @@
+function UserGreeting(props){
+
+    const welcomeMessage = <h2 className="welcome-message">welcome {props.username}</h2>
+    const loginPrompt = <h2 className="login-prompt">please login to continue</h2>
+
+    return(props.isLoggedIn ? welcomeMessage : loginPrompt);
+
+}
+export default UserGreeting
